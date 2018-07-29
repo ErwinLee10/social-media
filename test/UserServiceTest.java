779372e63e7d31779b0c31a12b976d40c2dd400f
Erwin Lee <erwin@mokapos.com>
2018-07-29 11:58:43 +0800

    add exception test cases

--- test/UserServiceTest.java
+++ test/UserServiceTest.java
@@ -20,7 +20,11 @@ import dtos.BlockUpdateDTO;
 import dtos.CreateFriendConnectionDTO;
 import dtos.GetUsersCanReceiveUpdateDTO;
 import dtos.SubscribeToUpdateDTO;
+import exceptions.NotAllowToAddFriendDueToBlockUpdateException;
+import exceptions.RequestPayloadException;
+import exceptions.UserNotFoundException;
 import models.FriendConnection;
+import models.SubscriptionStatus;
 import models.User;
 import models.UserUpdateSubscription;
 import repositories.IFriendConnectionRepository;
@@ -47,6 +51,7 @@ public class UserServiceTest {
 	
 	@InjectMocks private UserServiceImpl userService;
 	
+	/*****************************  createFriendConnection  ************************************/
 	@Test
 	public void testCreateFriendConnectionSuccess() {
 	  User user = new User();
@@ -67,7 +72,70 @@ public class UserServiceTest {
 	  verify(userUpdateSubscriptionRepository, times(2)).queryUnique(any(UserUpdateSubscriptionSpecification.class));
 	  verify(friendRepository, times(2)).queryUnique(any(FriendConnectionSpecification.class));
 	}
+	
+	@Test(expected = RequestPayloadException.class)
+	public void testCreateFriendConnectionFailedRequestPayloadException() { 
+	  CreateFriendConnectionDTO dto = new CreateFriendConnectionDTO();
+	  List<String> friends = new ArrayList<>();
+	  friends.add("erwin@example.com");
+	  friends.add("kamu@example.com");
+	  friends.add("kamu1@example.com");
+	  dto.setFriends(friends);
+	  
+	  
+	  userService.createFriendConnection(dto); 
+	  verify(userRepository, times(0)).queryUnique(any(UserSpecification.class));
+	  verify(userUpdateSubscriptionRepository, times(0)).queryUnique(any(UserUpdateSubscriptionSpecification.class));
+	  verify(friendRepository, times(0)).queryUnique(any(FriendConnectionSpecification.class));
+	}
+	
+	@Test(expected = UserNotFoundException.class)
+	public void testCreateFriendConnectionFailedUserNotFoundException() { 
+	  CreateFriendConnectionDTO dto = new CreateFriendConnectionDTO();
+	  List<String> friends = new ArrayList<>();
+	  friends.add("erwin@example.com");
+	  friends.add("kamu@example.com");
+	  dto.setFriends(friends);
+	  
+	  when(userRepository.queryUnique(any(UserSpecification.class))).thenThrow(new UserNotFoundException());
+	  
+	  userService.createFriendConnection(dto); 
+	  verify(userRepository, times(1)).queryUnique(any(UserSpecification.class));
+	  verify(userUpdateSubscriptionRepository, times(0)).queryUnique(any(UserUpdateSubscriptionSpecification.class));
+	  verify(friendRepository, times(0)).queryUnique(any(FriendConnectionSpecification.class));
+	}
 
+	@Test(expected = NotAllowToAddFriendDueToBlockUpdateException.class)
+	public void testCreateFriendConnectionFailedNotAllowToAddFriendDueToBlockUpdateException() { 
+	  CreateFriendConnectionDTO dto = new CreateFriendConnectionDTO();
+	  List<String> friends = new ArrayList<>();
+	  friends.add("erwin@example.com");
+	  friends.add("kamu@example.com");
+	  dto.setFriends(friends);
+	  
+	  User requestor = new User();
+	  requestor.setEmail("erwin@example.com");
+	  
+	  User target = new User();
+	  target.setEmail("kamu@example.com");
+	  
+	  UserUpdateSubscription userUpdateSubscription = new UserUpdateSubscription();
+	  userUpdateSubscription.setRequestor(requestor);
+	  userUpdateSubscription.setTarget(target);
+	  userUpdateSubscription.setSubscriptionStatus(SubscriptionStatus.BLOCKED);
+	  
+	  when(userRepository.queryUnique(any(UserSpecification.class))).thenReturn(requestor);
+	  when(userUpdateSubscriptionRepository.queryUnique(any(UserUpdateSubscriptionSpecification.class))).thenReturn(userUpdateSubscription);
+	  
+	  userService.createFriendConnection(dto); 
+	  verify(userRepository, times(2)).queryUnique(any(UserSpecification.class));
+	  verify(userUpdateSubscriptionRepository, times(2)).queryUnique(any(UserUpdateSubscriptionSpecification.class));
+	  verify(friendRepository, times(0)).queryUnique(any(FriendConnectionSpecification.class));
+	}
+	
+	
+	/*****************************  getUserFriends  ************************************/
+	
 	@Test
 	public void testGetUserFriendsSuccess() {
 	  String friendEmail = "friend@example.com";
@@ -94,7 +162,28 @@ public class UserServiceTest {
 	  verify(userRepository, times(1)).queryUnique(any(UserSpecification.class));
 	}
 	
+	@Test(expected = UserNotFoundException.class)
+	public void testGetUserFriendsFailedUserNotFoundException() {
+	  String friendEmail = "friend@example.com";
+	  User user = new User();
+	  user.setEmail("erwin@example.com");
+	 
+	  List<FriendConnection> friends = new ArrayList<>();
+	  User friend = new User();
+	  friend.setEmail(friendEmail);
+	  FriendConnection friendConnection = new FriendConnection();
+	  friendConnection.setFriend(friend);
+	  friends.add(friendConnection);
+	  
+	  user.setFriendsConnection(friends);
+	 
+	  when(userRepository.queryUnique(any(UserSpecification.class))).thenThrow(new UserNotFoundException());
+	  userService.getUserFriends("erwin@example.com"); 
+	  verify(userRepository, times(1)).queryUnique(any(UserSpecification.class));
+	}
 	
+	
+	/*****************************  findCommonFriends  ************************************/
 	@Test
 	public void testfindCommonFriendsSuccess() {
 	  User friend = new User();
@@ -114,7 +203,8 @@ public class UserServiceTest {
 	  assertEquals(userService.findCommonFriends(emails), resp); 
 	  verify(userRepository, times(1)).query(any(CommonFriendSpecification.class));
 	}
-	
+		
+	/*****************************  subscribeToUpdate  ************************************/
 	@Test
 	public void testSubscribeToUpdateSuccess() {
 	  SubscribeToUpdateDTO dto = new SubscribeToUpdateDTO();
@@ -128,6 +218,7 @@ public class UserServiceTest {
 	  verify(userRepository, times(2)).queryUnique(any(UserSpecification.class));
 	}
 	
+	/*****************************  blockUpdate  ************************************/
 	@Test
 	public void testBlockUpdateSuccess() {
 	  BlockUpdateDTO dto = new BlockUpdateDTO();
@@ -141,6 +232,7 @@ public class UserServiceTest {
 	  verify(userRepository, times(2)).queryUnique(any(UserSpecification.class));
 	}
 	
+	/*****************************  getUsersCanReceiveUpdateFrom  ************************************/
 	@Test
 	public void testgetUsersCanReceiveUpdateFromSuccess() {
 	GetUsersCanReceiveUpdateDTO dto = new GetUsersCanReceiveUpdateDTO();
@@ -190,6 +282,50 @@ public class UserServiceTest {
 	  resp.setRecipients(emails);
 	  
 	  assertEquals(userService.getUsersCanReceiveUpdateFrom(dto), resp);
+	  verify(userRepository, times(1)).queryUnique(any(UserSpecification.class));
+	}
+	
+	@Test(expected = UserNotFoundException.class)
+	public void testgetUsersCanReceiveUpdateFromFailedUserNotFoundException() {
+	GetUsersCanReceiveUpdateDTO dto = new GetUsersCanReceiveUpdateDTO();
+	  dto.setSender("erwin@example.com");
+	  dto.setText("target@example.com");
+	  
+	  User friend = new User();
+	  friend.setEmail("friend@example.com");
+	  
+	  FriendConnection friendConnection = new FriendConnection();
+	  friendConnection.setFriend(friend);
+	  
+	  List<FriendConnection> friendConnections = new ArrayList<>();
+	  friendConnections.add(friendConnection);
+	  
+	  User hater = new User();
+	  hater.setEmail("friend@example.com");
+	  
+	  UserUpdateSubscription haterSubscriber = new UserUpdateSubscription();
+	  haterSubscriber.setRequestor(hater);
+	  
+	  List<UserUpdateSubscription> haters = new ArrayList<>();
+	  haters.add(haterSubscriber);
+	  
+	  User fan = new User();
+	  fan.setEmail("fans@example.com");
+	  
+	  UserUpdateSubscription fansSubscriber = new UserUpdateSubscription();
+	  fansSubscriber.setRequestor(fan);
+	  
+	  List<UserUpdateSubscription> fans = new ArrayList<>();
+	  fans.add(fansSubscriber);
+	  
+	  User user = new User();
+	  user.setFriendsConnection(friendConnections);
+	  user.setMyHaters(haters);
+	  user.setMyFans(fans);
+	  
+	  
+	  when(userRepository.queryUnique(any(UserSpecification.class))).thenThrow(new UserNotFoundException());	  
+	  userService.getUsersCanReceiveUpdateFrom(dto);
 	  verify(userRepository, times(1)).queryUnique(any(UserSpecification.class));
 	}
 
